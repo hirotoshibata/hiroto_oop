6490893fd373d8ac1ec2dd8acc24791b15341dfe
柴田展大 <hi.iniesta111018@gmail.com>
2019-06-04 18:18:21 +0800

    宿題2が終了した。
    やり方ミスってたので改善した。

--- index.php
+++ index.php
@@ -1,65 +1,69 @@
 <?php
 
-$kid = new Kid();
+    $kid = new Kid();
 
-$kid->setSex('rady');
+    $kid->setSex('rady');
 
-$kid->setName('理香子');
+    $kid->setName('理香子');
 
-$kid->setAge(15);
+    $kid->setAge(8);
 
-$kid->showName();
-
-
-class Kid{
-    public $name;
-    public $sex;
-    public $age;
-
-    function __construct(){
-        $this->name = 'Seed';
-        $this->sex = 'boy';
-        $this->age = 10;
-    }
-
-    public function setSex($seibetu){
-        $this->sex=$seibetu;
-    }
-
-    public function getSex(){
-        return $this->sex;
-    }
-// ここまでは性別と初期値の設定なのであっている。
+    $kid->showName();
 
 
-// 男性の時と女性の時で条件分岐をさせたい。
-    public function setName($namae){
-        $this->name=$namae;
-    }
+    class Kid{
+        public $name;
+        public $sex;
+        public $age;
 
-    public function getName(){
-        return $this->name;
-    }
-
-    public function setAge($nennrei){
-        $this->age=$nennrei;
-    }
-
-    public function getAge(){
-        return $this->age;
-    }
-
-    public function showName(){
-        if ($this->age>=12) {
-            if($this->sex== 'boy'){
-                    echo "$this->name".'くん';
-                }elseif($this->sex== 'rady'){
-                    echo "$this->name".'ちゃん';
-            }
+        function __construct(){
+            $this->name = 'Seed';
+            $this->sex = 'boy';
+            $this->age = 10;
         }
+
+        public function setSex($seibetu){
+            $this->sex=$seibetu;
+        }
+
+        public function getSex(){
+            return $this->sex;
+        }
+    // ここまでは性別と初期値の設定なのであっている。
+
+
+    // 男性の時と女性の時で条件分岐をさせたい。
+        public function setName($namae){
+            $this->name=$namae;
+        }
+
+        public function getName(){
+            return $this->name;
+        }
+
+        public function setAge($nennrei){
+            $this->age=$nennrei;
+        }
+
+        public function getAge(){
+            return $this->age;
+        }
+    // ここまでで、セットなどの条件を全て書き出している。
+        public function showName(){
+            if ($this->age>=12) {
+                if($this->sex== 'boy'){
+                        echo "$this->name".'君';
+                    }elseif($this->sex== 'rady'){
+                        echo "$this->name".'さん';
+                }
+            }elseif($this->age<=12){
+                if($this->sex== 'boy'){
+                        echo "$this->name".'くん';
+                    }elseif($this->sex== 'rady'){
+                        echo "$this->name".'ちゃん';
+                }
+        }
+
     }
 
 }
-
-
-
