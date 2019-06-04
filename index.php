51bfaeedd000a1cd12218e4a477a0b8c694ee162
柴田展大 <hi.iniesta111018@gmail.com>
2019-06-04 17:38:57 +0800

    問題2回答終了
    問題を解いてブランチをした。

--- index.php
+++ index.php
@@ -6,16 +6,20 @@ $kid->setSex('rady');
 
 $kid->setName('理香子');
 
+$kid->setAge(15);
+
 $kid->showName();
 
 
 class Kid{
     public $name;
     public $sex;
+    public $age;
 
     function __construct(){
         $this->name = 'Seed';
         $this->sex = 'boy';
+        $this->age = 10;
     }
 
     public function setSex($seibetu){
@@ -29,7 +33,7 @@ class Kid{
 
 
 // 男性の時と女性の時で条件分岐をさせたい。
-        public function setName($namae){
+    public function setName($namae){
         $this->name=$namae;
     }
 
@@ -37,13 +41,23 @@ class Kid{
         return $this->name;
     }
 
-    public function showName(){
-        if($this->sex== 'boy'){
-                echo "$this->name".'くん';
-        }elseif($this->sex== 'rady'){
-        echo "$this->name".'ちゃん';
+    public function setAge($nennrei){
+        $this->age=$nennrei;
     }
-}
+
+    public function getAge(){
+        return $this->age;
+    }
+
+    public function showName(){
+        if ($this->age>=12) {
+            if($this->sex== 'boy'){
+                    echo "$this->name".'くん';
+                }elseif($this->sex== 'rady'){
+                    echo "$this->name".'ちゃん';
+            }
+        }
+    }
 
 }
 
