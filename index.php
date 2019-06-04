a74e04c3cdcebd29d620f8fc5deaa836acdd692b
柴田展大 <hi.iniesta111018@gmail.com>
2019-06-04 15:42:19 +0800

    問1（内容）
    作業途中（理由、内訳）

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+
+$kid = new Kid();
+
+
+$kid->showSex();
+
+
+
+
+class Kid{
+    // 時速の値を保持するプロパティ
+    public $name;
+    public $sex;
+
+    function __construct(){
+        $this->name = 'Seed';
+        $this->sex = 'boy';
+    }
+
+    public function showSex($seibetu){
+        if($seibetu== 'boy'){
+            echo $this->name;
+        }elseif($seibetu== 'rady'){
+            echo $this->name;
+        }
+    }
+
+    public function setName($namae){
+        $this->name=$namae
+    }
+
+    // public function getSpeed(){
+    //     return $this->speed;
+    // }
+
+ 
+
+    // public function greeting(){
+    //     $this->start();
+    //     $this->run();
+    //     $this->stop();
+    
+
+
+}
+
+// $car = new Car();
+
+// $car->setSpeed(60);
+
+// $car->greeting();
+
+
+
+
